c33fc256ba64e4b2a2a41e9c75db0a109ddf0709
Kuma-P <kumampet@cho-kagufan.com>
2026-07-14 23:59:56 +0900

    ターボパックを使わない

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Edge ランタイムのチャンク結合を安定化させるための設定
+  serverExternalPackages: [],
 };
 
 export default nextConfig;
